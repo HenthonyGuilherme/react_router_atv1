40a50badf8ff38fb08f79c80dde0eb03dc81f16d
HenthonyGuilherme <87839692+HenthonyGuilherme@users.noreply.github.com>
2021-11-21 17:46:42 -0300

    Add files via upload

---
+++ src/Cidade.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+
+
+
+export const Cidade = () =>{
+     
+    <div>
+        
+       <h1>Minha Cidade Favorita</h1>
+
+     <img className="imagem" src="https://cdn.getyourguide.com/img/location/5ffeb0b587f00.jpeg/88.jpg" />
+
+     <Link to ="/home" className="">Ir para Home</Link>
+         
+
+    </div>
+
+}
---
+++ src/Musica.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+
+
+export const Musica = () =>{
+     
+    <div>
+        
+       <h1>Minha Música Favorita</h1>
+
+       <a src="https://www.youtube.com/watch?v=kIBvhN21JIo&t=384s" target="_blank" ></a>
+
+       <Link to ="/home" className="">Ir para Home</Link>
+         
+
+    </div>
+
+}
